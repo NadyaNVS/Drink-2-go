a9e62720f06542496d7356581b5d5edfb9194e74
Nadezhda Strelnikova <nvs.strelnikova@gmail.com>
2023-04-27 12:36:46 +0300

    Updates

---
+++ js/slider.js
@@ -0,0 +1,1 @@
+const mySwiper=new Swiper(".swiper",{direction:"horizontal",loop:!0,slidesPerView:1,pagination:{el:".swiper-pagination",clickable:!0},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
